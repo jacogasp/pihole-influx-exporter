33c8fafe1d84a1d90482054c899f63fd3f0b2412
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-01-02 17:30:10 +0000

    Rename metrics with pihole prefix

--- src/influxdb.cpp
+++ src/influxdb.cpp
@@ -14,22 +14,23 @@ auto make_summary(Data const& data, std::string_view host)
 {
   influxdb::Point summary{"summary"};
   summary.addTag("host", host);
-  summary.addField("ads_blocked_today", data.ads_blocked_today);
-  summary.addField("ads_percentage_today", data.ads_percentage_today);
-  summary.addField("clients_ever_seen", data.clients_ever_seen);
-  summary.addField("dns_queries_all_replies", data.dns_queries_all_replies);
-  summary.addField("dns_quries_today", data.dns_queries_today);
-  summary.addField("domains_being_blocked", data.domains_being_blocked);
-  summary.addField("queries_cached", data.queries_cached);
-  summary.addField("queries_forwarded", data.queries_forwarded);
-  summary.addField("unique_clients", data.unique_clients);
-  summary.addField("unique_domains", data.unique_domains);
+  summary.addField("pihole_ads_blocked_today", data.ads_blocked_today);
+  summary.addField("pihole_ads_percentage_today", data.ads_percentage_today);
+  summary.addField("pihole_clients_ever_seen", data.clients_ever_seen);
+  summary.addField("pihole_dns_queries_all_replies",
+                   data.dns_queries_all_replies);
+  summary.addField("pihole_dns_quries_today", data.dns_queries_today);
+  summary.addField("pihole_domains_being_blocked", data.domains_being_blocked);
+  summary.addField("pihole_queries_cached", data.queries_cached);
+  summary.addField("pihole_queries_forwarded", data.queries_forwarded);
+  summary.addField("pihole_unique_clients", data.unique_clients);
+  summary.addField("pihole_unique_domains", data.unique_domains);
   return summary;
 }
 
 auto make_top_queries(Data const& data, std::string_view host)
 {
-  influxdb::Point top_queries{"top_queries"};
+  influxdb::Point top_queries{"pihole_top_queries"};
   top_queries.addTag("host", host);
   for (auto&& query : data.top_queries) {
     top_queries.addField(query.first, query.second);
@@ -39,7 +40,7 @@ auto make_top_queries(Data const& data, std::string_view host)
 
 auto make_top_ads(Data const& data, std::string_view host)
 {
-  influxdb::Point top_ads{"top_queries"};
+  influxdb::Point top_ads{"pihole_top_ads"};
   top_ads.addTag("host", host);
   for (auto&& ad : data.top_ads) {
     top_ads.addField(ad.first, ad.second);
@@ -49,7 +50,7 @@ auto make_top_ads(Data const& data, std::string_view host)
 
 auto make_top_sources(Data const& data, std::string_view host)
 {
-  influxdb::Point top_sources{"top_sources"};
+  influxdb::Point top_sources{"pihole_top_sources"};
   top_sources.addTag("host", host);
   for (auto&& source : data.top_sources) {
     top_sources.addField(source.first, source.second);
@@ -59,7 +60,7 @@ auto make_top_sources(Data const& data, std::string_view host)
 
 auto make_forward_destinations(Data const& data, std::string_view host)
 {
-  influxdb::Point forward_destinations{"forward_destionations"};
+  influxdb::Point forward_destinations{"pihole_forward_destionations"};
   forward_destinations.addTag("host", host);
   for (auto&& dest : data.forward_destinations) {
     forward_destinations.addField(dest.first, dest.second);
@@ -69,7 +70,7 @@ auto make_forward_destinations(Data const& data, std::string_view host)
 
 auto make_querytypes(Data const& data, std::string_view host)
 {
-  influxdb::Point querytypes{"querytypes"};
+  influxdb::Point querytypes{"pihole_querytypes"};
   querytypes.addTag("host", host);
   for (auto&& qt : data.querytypes) {
     querytypes.addField(qt.first, qt.second);
