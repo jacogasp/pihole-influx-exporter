e3918b81a9cc906ff3c40e44d84aa3dac91165d2
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-01-03 13:39:59 +0000

    Reorganize measurement and fields

--- src/influxdb.cpp
+++ src/influxdb.cpp
@@ -2,92 +2,102 @@
 
 #include <InfluxDB/InfluxDB.h>
 #include <InfluxDB/InfluxDBFactory.h>
+
 #include <fmt/format.h>
 #include <iostream>
+#include <string>
+
+auto constexpr MEASUREMENT_NAME = "pihole";
 
 InfluxDB::InfluxDB(std::string_view url, std::string_view hostname)
     : m_url{url}
     , m_host{hostname}
 {}
 
-auto make_summary(Data const& data, std::string_view host)
+void make_summary(auto influxdb, Data const& data, std::string_view host)
 {
-  influxdb::Point summary{"summary"};
-  summary.addTag("host", host);
-  summary.addField("status", data.status);
-  summary.addField("ads_blocked_today", data.ads_blocked_today);
-  summary.addField("ads_percentage_today", data.ads_percentage_today);
-  summary.addField("clients_ever_seen", data.clients_ever_seen);
-  summary.addField("dns_queries_all_replies", data.dns_queries_all_replies);
-  summary.addField("dns_quries_today", data.dns_queries_today);
-  summary.addField("domains_being_blocked", data.domains_being_blocked);
-  summary.addField("queries_cached", data.queries_cached);
-  summary.addField("queries_forwarded", data.queries_forwarded);
-  summary.addField("unique_clients", data.unique_clients);
-  summary.addField("unique_domains", data.unique_domains);
-  return summary;
+  influxdb::Point measure{MEASUREMENT_NAME};
+  measure.addTag("host", host);
+  measure.addField("status", data.status);
+  measure.addField("ads_blocked_today", data.ads_blocked_today);
+  measure.addField("ads_percentage_today", data.ads_percentage_today);
+  measure.addField("clients_ever_seen", data.clients_ever_seen);
+  measure.addField("dns_queries_all_replies", data.dns_queries_all_replies);
+  measure.addField("dns_quries_today", data.dns_queries_today);
+  measure.addField("domains_being_blocked", data.domains_being_blocked);
+  measure.addField("queries_cached", data.queries_cached);
+  measure.addField("queries_forwarded", data.queries_forwarded);
+  measure.addField("unique_clients", data.unique_clients);
+  measure.addField("unique_domains", data.unique_domains);
+  influxdb->write(std::move(measure));
 }
 
-auto make_top_queries(Data const& data, std::string_view host)
+void make_top_queries(auto influxdb, Data const& data, std::string_view host)
 {
-  influxdb::Point top_queries{"top_queries"};
-  top_queries.addTag("host", host);
   for (auto&& query : data.top_queries) {
-    top_queries.addField(query.first, query.second);
+    influxdb::Point measure{MEASUREMENT_NAME};
+    measure.addTag("host", host);
+    measure.addTag("domain", query.first);
+    measure.addField("top_queries", query.second);
+    influxdb->write(std::move(measure));
   }
-  return top_queries;
 }
 
-auto make_top_ads(Data const& data, std::string_view host)
+void make_top_ads(auto influxdb, Data const& data, std::string_view host)
 {
-  influxdb::Point top_ads{"top_ads"};
-  top_ads.addTag("host", host);
   for (auto&& ad : data.top_ads) {
-    top_ads.addField(ad.first, ad.second);
+    influxdb::Point measure{MEASUREMENT_NAME};
+    measure.addTag("host", host);
+    measure.addTag("domain", ad.first);
+    measure.addField("top_ads", ad.second);
+    influxdb->write(std::move(measure));
   }
-  return top_ads;
 }
 
-auto make_top_sources(Data const& data, std::string_view host)
+void make_top_sources(auto influxdb, Data const& data, std::string_view host)
 {
-  influxdb::Point top_sources{"top_sources"};
-  top_sources.addTag("host", host);
   for (auto&& source : data.top_sources) {
-    top_sources.addField(source.first, source.second);
+    influxdb::Point measure{MEASUREMENT_NAME};
+    measure.addTag("host", host);
+    measure.addTag("domain", source.first);
+    measure.addField("top_sources", source.second);
+    influxdb->write(std::move(measure));
   }
-  return top_sources;
 }
 
-auto make_forward_destinations(Data const& data, std::string_view host)
+void make_forward_destinations(auto influxdb, Data const& data,
+                               std::string_view host)
 {
-  influxdb::Point forward_destinations{"forward_destionations"};
-  forward_destinations.addTag("host", host);
   for (auto&& dest : data.forward_destinations) {
-    forward_destinations.addField(dest.first, dest.second);
+    influxdb::Point measure{MEASUREMENT_NAME};
+    measure.addTag("host", host);
+    measure.addTag("domain", dest.first);
+    measure.addField("forward_destinations", dest.second);
+    influxdb->write(std::move(measure));
   }
-  return forward_destinations;
 }
 
-auto make_querytypes(Data const& data, std::string_view host)
+void make_querytypes(auto influxdb, Data const& data, std::string_view host)
 {
-  influxdb::Point querytypes{"querytypes"};
-  querytypes.addTag("host", host);
   for (auto&& qt : data.querytypes) {
-    querytypes.addField(qt.first, qt.second);
+    influxdb::Point measure{MEASUREMENT_NAME};
+    measure.addTag("host", host);
+    measure.addTag("domain", qt.first);
+    measure.addField("querytypes", qt.second);
+    influxdb->write(std::move(measure));
   }
-  return querytypes;
 }
 
 void InfluxDB::send_data(Data const& data)
 {
   try {
     auto influxdb = influxdb::InfluxDBFactory::Get(m_url);
-    influxdb->write(make_summary(data, m_host));
-    influxdb->write(make_top_queries(data, m_host));
-    influxdb->write(make_top_ads(data, m_host));
-    influxdb->write(make_top_sources(data, m_host));
-    influxdb->write(make_forward_destinations(data, m_host));
-    influxdb->write(make_querytypes(data, m_host));
+    make_summary(influxdb.get(), data, m_host);
+    make_top_queries(influxdb.get(), data, m_host);
+    make_top_ads(influxdb.get(), data, m_host);
+    make_top_sources(influxdb.get(), data, m_host);
+    make_forward_destinations(influxdb.get(), data, m_host);
+    make_querytypes(influxdb.get(), data, m_host);
     print_data(data);
     std::cout << "data sent to influxdb." << std::endl;
   } catch (std::exception const& e) {
